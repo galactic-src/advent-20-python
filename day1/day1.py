d929188a85424772187b65b82e4a537b216ebb1a
Jonny <jonnym22@gmail.com>
2020-12-01 08:51:06 +0000

    day 1

--- day1/day1.py
+++ day1/day1.py
@@ -1,27 +1,29 @@
 import collections
 from enum import Enum, auto
+import math
 import re
 from itertools import combinations, accumulate
 
 from util import ManyLineInput, OneLineInput, windowed
 
 
+def solve(inputs, count):
+    for selection in combinations(inputs, count):
+        if sum(selection) == 2020:
+            return math.prod(selection)
+
+
 def part1():
-    # with ManyLineInput('./input.txt') as data:
-    with OneLineInput('./input.txt') as data:
-        answer = ''
-        print(f"part 1: {answer}")
+    with ManyLineInput('./input.txt', int) as data:
+        print(f"part 1: {solve(data, 2)}")
 
 
 def part2():
-    # with ManyLineInput('./input.txt') as data:
-    with OneLineInput('./input.txt') as data:
-        answer = ''
-        print(f"part 2: {answer}")
+    with ManyLineInput('./input.txt', int) as data:
+        print(f"part 2: {solve(data, 3)}")
 
 
 if __name__ == "__main__":
-    pass
-    # part1()
-    # part2()
+    part1()
+    part2()
 
