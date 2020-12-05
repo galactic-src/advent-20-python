39133997adaa2e3a229d04a641b23781cb1f4daf
Jonny <jonnym22@gmail.com>
2020-12-05 08:20:16 +0000

    day 5

--- day5/day5.py
+++ day5/day5.py
@@ -1,27 +1,33 @@
-import collections
-from enum import Enum, auto
-import re
-from itertools import combinations, accumulate
+from util import ManyLineInput
 
-from util import ManyLineInput, OneLineInput, windowed
+
+def sum_pow2(s, on):
+    return sum(map(lambda ic: pow(2, len(s) - ic[0] - 1) * int(ic[1] == on), enumerate(s)))
+
+
+class BoardingPass:
+    def __init__(self, s):
+        self.row = sum_pow2(s[:7], 'B')
+        self.col = sum_pow2(s[7:], 'R')
+
+    def seat_id(self):
+        return 8 * self.row + self.col
 
 
 def part1():
-    # with ManyLineInput('./input.txt') as data:
-    with OneLineInput('./input.txt') as data:
-        answer = ''
+    with ManyLineInput('./input.txt', BoardingPass) as data:
+        answer = max(map(lambda bp: bp.seat_id(), data))
         print(f"part 1: {answer}")
 
 
 def part2():
-    # with ManyLineInput('./input.txt') as data:
-    with OneLineInput('./input.txt') as data:
-        answer = ''
-        print(f"part 2: {answer}")
+    with ManyLineInput('./input.txt', BoardingPass) as data:
+        seat_ids = {bp.seat_id() for bp in data}
+        answer = set(range(min(seat_ids),  max(seat_ids))) - seat_ids
+        print(f"part 2: {list(answer)[0]}")
 
 
 if __name__ == "__main__":
-    pass
-    # part1()
-    # part2()
+    part1()
+    part2()
 
