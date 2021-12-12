e79f05644a8b3bc25dc3fdb05a4354849603fdf8
Jonny <jonnym22@gmail.com>
2021-12-12 14:05:14 +0000

    day9 -> day09

---
+++ 2021/day13/day13.py
@@ -0,0 +1,25 @@
+import collections
+from enum import Enum, auto
+import re
+from itertools import combinations, accumulate
+
+from util import ManyLineInput, OneLineInput, windowed, DelimitedLinesBlockInput
+
+
+def part1():
+    with ManyLineInput('./input.txt') as data:
+
+        answer = ''
+        print(f"part 1: {answer}")
+
+
+def part2():
+    with ManyLineInput('./input.txt') as data:
+    # with OneLineInput('./day23.py') as data:
+        answer = ''
+        print(f"part 2: {answer}")
+
+
+if __name__ == "__main__":
+    part1()
+    part2()
